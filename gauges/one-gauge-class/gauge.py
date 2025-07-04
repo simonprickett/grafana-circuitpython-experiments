93873fac298c1b85ca5b64456d5f89916b2f83a2
Simon Prickett <simon.prickett@grafana.com>
2025-07-04 13:12:26 +0100

    Added basic timing function for smoother motion.

--- gauges/one-gauge-class/gauge.py
+++ gauges/one-gauge-class/gauge.py
@@ -1,4 +1,6 @@
+import math
 import pwmio
+import time
 
 class Gauge:
     MAX_DUTY_CYCLE = 60000
@@ -10,19 +12,34 @@ class Gauge:
 
     def __percent_to_duty_cycle(self, pct):    
         return int((pct / 100) * self.MAX_DUTY_CYCLE)
+    
+    
+    # Source from: https://gist.github.com/robweychert/7efa6a5f762207245646b16f29dd6671
+    def __easeInOutSine(t):
+        return -(math.cos(math.pi * t) -1) / 2
 
 
-    def set_percentage(self, new_pct, smooth_transition = False):
+    def set_percentage(self, new_pct, with_easing = False):
         if new_pct > 100:
             new_pct = 100
         elif new_pct < 0:
             new_pct = 0
 
-        if smooth_transition == True:
-            print("TODO smooth transitions!")
+        # If there's no change, do nothing.
+        if new_pct == self.current_pct:
+            return
+
+        if with_easing == True:
+            num_frames = abs(self.current_pct - new_pct)
+            print(f"Running transition from {self.current_pct} to {new_pct} ({num_frames} frames).")
+            incr = -1 if new_pct < self.current_pct else 1
+
+            for f in range(0, num_frames):
+                self.current_pct = self.current_pct + incr                
+                self.gauge.duty_cycle = self.__percent_to_duty_cycle(self.current_pct)
+                time.sleep(0.05)
+                
+            print(f"End pct {self.current_pct}")
         else:
             self.gauge.duty_cycle = self.__percent_to_duty_cycle(new_pct)
-
-        self.current_pct = new_pct
-        
-
+            self.current_pct = new_pct
